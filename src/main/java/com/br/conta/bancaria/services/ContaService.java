bcfe31810253a28b80199d6c5b5f50b6171a3cb8
jeffersonbaptista <jefferson.baptista@zup.com.br>
2019-07-23 23:17:32 -0300

    criar metodos apiRest

--- src/main/java/com/br/conta/bancaria/services/ContaService.java
+++ src/main/java/com/br/conta/bancaria/services/ContaService.java
@@ -12,24 +12,52 @@ import com.br.conta.bancaria.repositories.ContaRepopsitory;
 
 @Service
 public class ContaService {
-	
+
 	@Autowired
 	private ContaRepopsitory contaRepopsitory;
-	
+
 	@Autowired
-	private ClienteRepository clienteRepository;		
-	
-	public Iterable<Conta> exibirTodasContas(){
+	private ClienteRepository clienteRepository;
+
+	public Iterable<Conta> exibirTodasContas() {
 		return contaRepopsitory.findAll();
 	}
-	
+
 	public String salvarConta(Cliente cliente, Conta conta) {
-		
+
 		conta.setCliente(cliente);
-		contaRepopsitory.save(conta);		
-		
+		contaRepopsitory.save(conta);
+
 		return "Conta criada com sucesso";
-		
+
 	}
+
+	public long totalContas() {
+		return contaRepopsitory.count();
+	}
+
+	public void salvarContaApiRest(Conta conta) {
+		contaRepopsitory.save(conta);
+	}
+	
+	
+	
+	 public void atualizarContaApiRest(int id,Conta conta) {
+		 conta.setId(id);
+		 contaRepopsitory.save(conta);
+		 
+	 }
+	 public void cancelarContaApiRest(int id) {
+		 contaRepopsitory.deleteById(id);
+		 
+	 }
+	 
+	 public Conta pegarContaIdApiRest(int id) {
+		 
+		 if(contaRepopsitory.existsById(id)) {
+		 return contaRepopsitory.findById(id).get();
+		 }
+		 return null;
+	 }
 
 }
